3babf5a9ea242e7575764c2a5a42b33621c78bb7
Hitesh <hiteshpatodia1@gmail.com>
2025-04-22 18:48:34 +0530

    Dashboard Count of Pharmacy and products API and service

--- RepMed.Services/IProductService.cs
+++ RepMed.Services/IProductService.cs
@@ -16,6 +16,7 @@ namespace RepMed.Services
     public interface IProductService : IDisposable
     {
         Task<APIsResponse<Datatable<ProductsPagingResponse>>> GetProducts(ProductsPagingRequest reqDto);
+        Task<APIsResponse<DashboardDto>> GetCount();
     }
 
     public class ProductService : BaseService, IProductService
@@ -28,6 +29,32 @@ namespace RepMed.Services
         public void Dispose()
         {
             GC.SuppressFinalize(this);
+        }
+
+        public async Task<APIsResponse<DashboardDto>> GetCount()
+        {
+            try
+            {
+                APIsResponse<Datatable<DashboardDto>> apiResponse = default;
+                #region Get All Pharmacy 
+                var parameters = new DynamicParameters();
+                var result = (await _idbConnection.QueryAsync<DashboardDto>(
+                               sql: "GET_COUNT",
+                               commandType: CommandType.StoredProcedure,
+                               transaction: _idbTransaction
+                )).FirstOrDefault();
+                #endregion
+                
+                if (result!=null)
+                    return await Task.FromResult(new APIsSuccsss<DashboardDto>(_validateMessages.RetriveSuccess, result));
+                else
+                    return await Task.FromResult(new APIsSuccsss<DashboardDto>(_validateMessages.NotExist));
+
+            }
+            catch (Exception ex)
+            {
+                return await Task.FromResult(new APIsError<DashboardDto>(ex.GetActualError()));
+            }
         }
 
         public async Task<APIsResponse<Datatable<ProductsPagingResponse>>> GetProducts(ProductsPagingRequest reqDto)
