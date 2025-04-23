a6b2d32e1692b346afad90a6e2d6f77d3cc929c3
Hitesh <hiteshpatodia1@gmail.com>
2025-04-23 10:55:04 +0530

    minor change

--- RepMed.Web/Controllers/WebApis/ProductsController.cs
+++ RepMed.Web/Controllers/WebApis/ProductsController.cs
@@ -54,7 +54,7 @@ namespace RepMed.Web.Controllers.WebApis
                         var result = await productService.GetCount();
                         if (!result.IsSuccess)
                             return BadRequest(new APIsResponse<string> { IsSuccess = false, Message = result.Message });
-                        return Ok(new APIsResponse<DashboardDto> { IsSuccess = false, Message = result.Message, Data= result.Data });
+                        return Ok(new APIsResponse<DashboardDto> { IsSuccess = true, Message = result.Message, Data= result.Data });
                     }
                 }
             }
