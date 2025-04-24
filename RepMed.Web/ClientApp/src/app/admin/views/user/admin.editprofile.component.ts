ba00a4a89d2c86ae3e9bf7dc5c8c19e5ad238746
Ganesh <ganesh.saini@arkasoftwares.com>
2025-04-24 17:10:33 +0530

    add changes for paste number in field

--- RepMed.Web/ClientApp/src/app/admin/views/user/admin.editprofile.component.ts
+++ RepMed.Web/ClientApp/src/app/admin/views/user/admin.editprofile.component.ts
@@ -228,6 +228,11 @@ export class EditProfile extends AdminBaseComponent implements OnInit {
         }
     }
 
+    disablePaste(event: ClipboardEvent): void {
+        event.preventDefault();
+      }
+    
+
     allowOnlyNumbers(event: KeyboardEvent) {
         const charCode = event.key.charCodeAt(0);
         if (charCode < 48 || charCode > 57) {
