700935e9c6b39265924f22cb86d5fd97e271a74d
Hitesh <hiteshpatodia1@gmail.com>
2025-05-02 14:41:41 +0530

    Chnage In Login Api response send role based permissions of moduel and submodules and changed in DB

--- RepMed.Dtos/PermisseionDto.cs
+++ RepMed.Dtos/PermisseionDto.cs
@@ -10,11 +10,32 @@ namespace RepMed.Dtos
     {
         public string Module { get; set; }
         public string Route { get; set; }
+        public string Label { get; set; }
     }
-    public class EntityPermissionDto: BasePermissionDto
+    public class EntityPermissionDto : BasePermissionDto
     {
         public long Id { get; set; }
-       
     }
+    public class PermissionJoinDto
+    {
+        public int ParentId { get; set; }
+        public string ParentModule { get; set; }
+        public string ParentRoute { get; set; }
+        public string ParentLabel { get; set; }
+        public string ChildRoute { get; set; }
+        public string ChildLabel { get; set; }
+    }
+    public class PermissionChildDto
+    {
+        public string Label { get; set; }
+        public string Route { get; set; }
+    }
+    public class PermissionParentDto
+    {
+        public string Label { get; set; } // Parent Label
+        public string Route { get; set; } // Parent Label
+        public List<PermissionChildDto> Children { get; set; } = new();
+    }
+
 
 }
