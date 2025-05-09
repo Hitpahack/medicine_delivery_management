79f86af45665e996a2c64bb5031e3c8931b7e2d7
Hitesh <hiteshpatodia1@gmail.com>
2025-05-09 17:38:07 +0530

    Minor change

--- RepMed.Dtos/FAQ/FAQPagingResponse.cs
+++ RepMed.Dtos/FAQ/FAQPagingResponse.cs
@@ -8,6 +8,11 @@ namespace RepMed.Dtos.FAQ
 {
     public class FAQPagingResponse : SP_BASE_RESPONSE
     {
-
+        public int Id { get; set; }
+        public string Question { get; set; } 
+        public string Answer { get; set; } 
+        public bool IsActive { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
     }
 }
