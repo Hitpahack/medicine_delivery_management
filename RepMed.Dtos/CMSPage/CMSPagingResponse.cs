8c509d9044d1f7906549da014852daf8b6b491b7
Hitesh <hiteshpatodia1@gmail.com>
2025-05-09 11:26:54 +0530

    Minor changes

--- RepMed.Dtos/CMSPage/CMSPagingResponse.cs
+++ RepMed.Dtos/CMSPage/CMSPagingResponse.cs
@@ -11,6 +11,7 @@ namespace RepMed.Dtos.CMSPage
         public int Id { get; set; }
         public string Title { get; set; }
         public string Slug { get; set; }
+        public string Content { get; set; }
         public string MetaTitle { get; set; }
         public string MetaContext { get; set; }
         public bool IsActive { get; set; }
