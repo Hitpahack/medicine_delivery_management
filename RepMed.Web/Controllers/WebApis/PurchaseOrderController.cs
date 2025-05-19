3a274e222e5b85608bfa6ad7d3c52344a1efcc09
Hitesh Patodia <hiteshpatodia1@gmail.com>
2025-05-19 10:14:22 +0530

    minor change

--- RepMed.Web/Controllers/WebApis/PurchaseOrderController.cs
+++ RepMed.Web/Controllers/WebApis/PurchaseOrderController.cs
@@ -274,7 +274,7 @@ namespace RepMed.Web.Controllers.WebApis
                         {
                             return BadRequest(result);
                         }
-                        return Ok(result);
+                        return Ok(result.Data);
                     }
                 }
             }
